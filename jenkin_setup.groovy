6a4845a2da52c4b551fbcc6c5eb67fec55c7754c
Vardhan Babu Duggineni <vardhanbabuduggineni@gmail.com>
2024-09-02 15:11:31 +0530

    Update jenkin_setup.groovy

--- jenkin_setup.groovy
+++ jenkin_setup.groovy
@@ -35,13 +35,3 @@ if (installed) {
 }
 
 
-import jenkins.model.*
-import hudson.security.*
-
-def instance = Jenkins.getInstance()
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount('admin','password')
-instance.setSecurityRealm(hudsonRealm)
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-instance.setAuthorizationStrategy(strategy)
-instance.save()
